6cb2090fcab2e7b4e7461732cbc615b172b6e1d8
ajtock <andytock@gmail.com>
2021-10-07 17:04:07 +0100

    update

---
+++ snakemake_BSseq_t2t-col.20210610/coverage/report/csmit_DMRcaller_hypo_met1.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+csmit -m 50G -c 25 "bash ./DMRcaller_hypo.sh 'WT_BSseq_Rep2_2013,WT_BSseq_Rep3_2013,WT_BSseq_Rep1_2014' 'met1_BSseq_Rep1' t2t-col.20210610 'Chr1,Chr2,Chr3,Chr4,Chr5' genomewide 6 CpG" & sleep 10;
+csmit -m 50G -c 25 "bash ./DMRcaller_hypo.sh 'WT_BSseq_Rep2_2013,WT_BSseq_Rep3_2013,WT_BSseq_Rep1_2014' 'met1_BSseq_Rep1' t2t-col.20210610 'Chr1,Chr2,Chr3,Chr4,Chr5' genomewide 6 CHG" & sleep 10;
+csmit -m 50G -c 25 "bash ./DMRcaller_hypo.sh 'WT_BSseq_Rep2_2013,WT_BSseq_Rep3_2013,WT_BSseq_Rep1_2014' 'met1_BSseq_Rep1' t2t-col.20210610 'Chr1,Chr2,Chr3,Chr4,Chr5' genomewide 6 CHH" & sleep 10;
